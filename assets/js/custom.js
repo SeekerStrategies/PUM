2f222676ef501e21ec6a1ecc1b8b6f2b31f911fd
Faizan <malikfaizan403@gmail.com>
2023-10-07 01:07:43 +0500

    made it mobile responsive.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,27 +1,19 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-    var banner = $('.banner').height();
-    if (scroll > banner) {
-        $("header").addClass("sticky_header");
-        $('#btn-back-to-top').fadeIn();
-    } else {
-        $("header").removeClass("sticky_header");
-        $('#btn-back-to-top').fadeOut();
+$(window).scroll(function() {  
+    if ($(window).width() > 992) {  
+        var scroll = $(window).scrollTop();
+        var banner = $('.banner').height();
+        if (scroll > banner) {
+            $("header").addClass("sticky_header");
+        } else {
+            $("header").removeClass("sticky_header");
+        }
     }
 });
 jQuery(document).ready(function() {
-    $(".navbar-toggler").click(function(){
-        $("body").addClass("overflow");
-    });
-    $(".menu_close, .nav-link").click(function(){
-        $("body").removeClass("overflow");
-    });
-    $(".menu_close, .nav-link").click(function(){
-        $("#collapsibleNavbar").removeClass("show");
-        $("#collapsibleNavbar").slideUp();
-    });
 
-
+    if ($(window).width() < 992) {
+        $('header').addClass('sticky_header');
+    }
     // Team Slider
     // Initialize Slick Slider for both content and images
     $(".slider-single").slick({
@@ -43,9 +35,3 @@ jQuery(document).ready(function() {
       });
       
 });
-// $('#btn-back-to-top').click(function () {
-//     $('body,html').animate({
-//         scrollTop: 0
-//     }, 400);
-//     return false;
-// });
